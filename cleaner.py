39bc31adc6c8d29315c8457950d1469d8738dfb6
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2019-10-30 23:47:44 +0700

    update is_valid_word

--- cleaner.py
+++ cleaner.py
@@ -18,9 +18,15 @@ def stem_if_not_noun(lexicon, word):
         return lexicon_entry.lemma
     return word.lower()
 
-def is_valid_word(word):
+def is_valid_mwe(word):
     count_non_alphas = len([c for c in word if not c.isalpha()])
     is_alphas_dominant = len(word) > 3 * count_non_alphas
     has_digit = any(c.isdigit() for c in word)
     has_strange_char = any(c in PUNCTUATION for c in word)
     return not has_digit and not has_strange_char and is_alphas_dominant
+
+def is_valid_word(word):
+    count_punc = len([c for c in word if c in string.punctuation])
+    count_digit = len(list(filter(str.isdigit, word)))
+    count_alpha = len(list(filter(str.isalpha, word)))
+    return len(word) > 5 * count_punc and (count_alpha * count_digit == 0 or count_digit == 1)
